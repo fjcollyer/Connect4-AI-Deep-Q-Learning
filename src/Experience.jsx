766717e6c4696b630a61dce49bf2d22d94aa229d
Fredrik James Collyer <collyerfj@gmail.com>
2024-01-13 01:12:18 +1100

    first commit starting with R3F code

---
+++ src/Experience.jsx
@@ -0,0 +1,63 @@
+import { shaderMaterial, Sparkles, Center, useTexture, useGLTF, OrbitControls } from '@react-three/drei'
+import * as THREE from 'three'
+import { useFrame, extend } from '@react-three/fiber'
+import { useRef } from 'react'
+import portalVertexShader from './shaders/portal/vertex.glsl'
+import portalFragmentShader from './shaders/portal/fragment.glsl'
+
+const PortalMaterial = shaderMaterial(
+    {
+        uTime: 0,
+        uColorStart: new THREE.Color('#ffffff'),
+        uColorEnd: new THREE.Color('#000000')
+    },
+    portalVertexShader,
+    portalFragmentShader
+)
+
+extend({ PortalMaterial })
+
+export default function Experience() {
+    const { nodes } = useGLTF('./model/portal.glb')
+
+    const bakedTexture = useTexture('./model/Baked4.jpg')
+    bakedTexture.flipY = false
+
+    const portalMaterial = useRef()
+
+    useFrame((state, delta) => {
+        portalMaterial.current.uTime += delta
+    })
+
+    return <>
+
+        <OrbitControls makeDefault />
+
+        <Center>
+            <mesh geometry={nodes.baked.geometry}>
+                <meshBasicMaterial map={bakedTexture} />
+            </mesh>
+
+            <mesh geometry={nodes.poleLightA.geometry} position={nodes.poleLightA.position}>
+                <meshBasicMaterial color="#ffffe5" />
+            </mesh>
+
+            <mesh geometry={nodes.poleLightB.geometry} position={nodes.poleLightB.position}>
+                <meshBasicMaterial color="#ffffe5" />
+            </mesh>
+
+            <mesh geometry={nodes.portalLight.geometry} position={nodes.portalLight.position} rotation={nodes.portalLight.rotation}>
+                <portalMaterial ref={portalMaterial} />
+            </mesh>
+
+            <Sparkles
+                size={6}
+                scale={[4, 2, 4]}
+                position-y={1}
+                speed={0.2}
+                count={40}
+            />
+        </Center>
+
+    </>
+}
---
+++ src/index.jsx
@@ -0,0 +1,21 @@
+import './style.css'
+import ReactDOM from 'react-dom/client'
+import { Canvas } from '@react-three/fiber'
+import Experience from './Experience.jsx'
+
+const root = ReactDOM.createRoot(document.querySelector('#root'))
+
+root.render(
+    <Canvas
+        flat
+        camera={ {
+            fov: 45,
+            near: 0.1,
+            far: 50,
+            position: [ 1, 2, 6 ]
+        } }
+    >
+        <color args={ [ '#030202' ] } attach="background" />
+        <Experience />
+    </Canvas>
+)
